212f7f603f4ce257e3eccd0d649c02c6a51d3c76
Ozan Can İĞCİ <oznigci@hotmail.com>
2023-10-08 22:01:53 +0300

    BloggieProjectDbContext class'ı yazıldı.

---
+++ BloggieProject.Entities/Contexts/BloggieProjectDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BloggieProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggieProject.Entities.Contexts
+{
+    public class BloggieProjectDbContext : DbContext
+    {
+        public BloggieProjectDbContext(DbContextOptions<BloggieProjectDbContext> options) : base(options) { }
+
+        public DbSet<BlogPost> BlogPosts  { get; set; }
+        public DbSet<Tag> Tags  { get; set; }
+        public DbSet<BlogPostLike> BlogPostLikes  { get; set; }
+
+    }
+}
